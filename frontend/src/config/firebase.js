d1a6e972b72e07f35ef591793b2b747710e98f07
gusttavori <gusta.contato@outlook.com>
2026-03-24 15:10:49 -0300

    Ajustando chaves

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -5,12 +5,12 @@ import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqeLJ_7KEKszVJrs4Q29ZJCdGyVgS1xpk",
-  authDomain: "pedeae-a50ce.firebaseapp.com",
-  projectId: "pedeae-a50ce",
-  storageBucket: "pedeae-a50ce.firebasestorage.app",
-  messagingSenderId: "1005982730734",
-  appId: "1:1005982730734:web:39be81d462253487f766ff"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 // Inicializa o Firebase
@@ -28,10 +28,10 @@ if (process.env.NODE_ENV === 'development') {
 
   // Usar 'localhost' é mais estável para comunicação via navegador (Web)
   connectFirestoreEmulator(db, 'localhost', 8080);
-  
+
   // O emulador de Functions
   connectFunctionsEmulator(functions, 'localhost', 5001);
-  
+
   // O emulador de Autenticação (Login)
   connectAuthEmulator(auth, "http://localhost:9099");
 }
